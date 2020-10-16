e9713dc708dc341ae1c1e272c409211519c46972
Mikhail Vintcukevich <mishavint@mail.ru>
2020-10-16 17:41:13 +0300

    Попытка номер 5 - поменял NULL на 0, везде, где это было нужно

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include <limits.h>
 //#include "sort.c"
 
-extern void work_with_array(long long* restrict array[] , int* number_of_elements);
+extern void work_with_array(long long* restrict array , int* number_of_elements);
 
 #define error(...) (fprintf(stderr, __VA_ARGS__))
 
@@ -20,7 +20,7 @@ int work_with_params (long long* restrict from , long long* restrict to , int nu
         return -2;
     }
     if(number_of_params > 2 && ( ((strncmp(params[1] , "--from=" , 7) ==0 )  && (strncmp(params[2] , "--from=" , 7) ==0))
-                        || ((strncmp(params[1] , "--to=" , 5) ==0 )  && (strncmp(params[2] , "--to=" , 5) ==0) ) )  )// Если есть два одинаковых параметра, то это ошибка (-3)
+                                 || ((strncmp(params[1] , "--to=" , 5) ==0 )  && (strncmp(params[2] , "--to=" , 5) ==0) ) )  )// Если есть два одинаковых параметра, то это ошибка (-3)
     {
         return -3;
     }
@@ -30,7 +30,7 @@ int work_with_params (long long* restrict from , long long* restrict to , int nu
     int second_flag_for_from = 1 , second_flag_for_to = 1;
     for (int i = 1 ; i < number_of_params ; ++i)                                        // Прогоняем циклом все параметры, которые получили из консоли
     {
-            if( (*(from) == NULL) && (strncmp(params[i]  , "--from=" , 7) == 0))     // Если нашли параметр, где начало строки совпадает с "--from=" то надо вычислить то, что нам надо из этого параметра
+        if( (*(from) == 0) && (strncmp(params[i]  , "--from=" , 7) == 0))     // Если нашли параметр, где начало строки совпадает с "--from=" то надо вычислить то, что нам надо из этого параметра
         {
             *(from) = strtoll(params[i] +7 , NULL, 10 );  // Вбиваем число в переменную
 
@@ -40,7 +40,7 @@ int work_with_params (long long* restrict from , long long* restrict to , int nu
                 second_flag_for_from *= 2;
             }
         }
-        else if( (*(to) == NULL)  && (strncmp( params[i]  , "--to=" , 5) == 0))    // Если нашли параметр, где начало строки совпадает с "--to=" то надо вычислить то, что нам надо из этого параметра
+        else if( (*(to) == 0)  && (strncmp( params[i]  , "--to=" , 5) == 0))    // Если нашли параметр, где начало строки совпадает с "--to=" то надо вычислить то, что нам надо из этого параметра
         {
             *(to) = strtoll(params[i] +5 , NULL, 10 );          // Вбиваем число в переменную
 
@@ -52,15 +52,15 @@ int work_with_params (long long* restrict from , long long* restrict to , int nu
         }
     }
 
-    if(((second_flag_for_from == 1 ) && (second_flag_for_to) ==1 ) && ((*(from) == NULL) && (*(to) == NULL) )) // Если 2 раза было введено некоректное число, то это ошибка (-4)
+    if(((second_flag_for_from == 1 ) && (second_flag_for_to) ==1 ) && ((*(from) == 0) && (*(to) == 0) )) // Если 2 раза было введено некоректное число, то это ошибка (-4)
     {
         return -4;
     }
-    else if(flag_for_from == 1 && ((*(to) != NULL) || (second_flag_for_to == 2)))       // Если пользователь намеренно не ввёл переменную, то нужно присвоить ей минимально возможное значение
+    else if(flag_for_from == 1 && ((*(to) != 0) || (second_flag_for_to == 2)))       // Если пользователь намеренно не ввёл переменную, то нужно присвоить ей минимально возможное значение
     {
         *(from) = LLONG_MIN;
     }
-    else if(flag_for_to == 1 && ((*(from) != NULL) || (second_flag_for_from == 2)))     // Если пользователь намеренно не ввёл переменую, то нужно ей присвоить максимально возможное значение
+    else if(flag_for_to == 1 && ((*(from) != 0) || (second_flag_for_from == 2)))     // Если пользователь намеренно не ввёл переменую, то нужно ей присвоить максимально возможное значение
     {
         *(to) = LLONG_MAX;
     }
@@ -68,8 +68,7 @@ int work_with_params (long long* restrict from , long long* restrict to , int nu
     return 0;           // Если всё прошло успешно, возвращаем 0
 }
 
-
-int string_to_array(long long* restrict from ,long long* restrict to, long long* restrict array[] , int max_size_of_array)
+int string_to_array(long long* restrict from ,long long* restrict to, long long* restrict array , int max_size_of_array)
 {
     int elements_in_array = 0;                                                          // Переменная, обозначающа яколичество цифр в массиве
     long long number;                                                                   // Временное число, которые мы проверим на принадлежность к (from , to)
@@ -101,8 +100,9 @@ int string_to_array(long long* restrict from ,long long* restrict to, long long*
     return elements_in_array;
 }
 
+
 int main(int argc, char** argv) {
-    long long int from = NULL, to  = NULL;                                  // Заполняем переменные нулями, пригодится дальшe
+    long long int from = 0, to  = 0;                                  // Заполняем переменные нулями, пригодится дальшe
     long long int array[100];
 
     int result_of_work = work_with_params(&from , &to , argc , argv );      // Создал функцию, которая заполняет переменные и возвращает возможные ошибки
@@ -111,7 +111,7 @@ int main(int argc, char** argv) {
         return result_of_work;                                              // Возвращаем ошибку и завершаем работу
     }
 
-    int elements_in_array = string_to_array(&from , &to, &array , 100);        // Получаем количество цифр(элементов) в строке, параллельно заполняя массив цифрами
+    int elements_in_array = string_to_array(&from , &to, array , 100);        // Получаем количество цифр(элементов) в строке, параллельно заполняя массив цифрами
 
     work_with_array(array , &elements_in_array );
 
