728dccd313765b81f230718dfc46353bf59d108d
Mikhail Vintcukevich <mishavint@mail.ru>
2020-10-19 01:24:42 +0300

    7.4

--- src/sort.c
+++ src/sort.c
@@ -1,15 +1,1 @@
-void work_with_array(long long* restrict array[] , int* number_of_elements)
-{
-    for(int i = 0 ; i < *(number_of_elements)-1 ; ++i)
-    {
-        for(int j = 0 ; j < *(number_of_elements) - i - 1 ; ++j)
-        {
-            if(array[j] > array[j+1])
-            {
-                long long int temp = *array[j];
-                array[j] = array[j+1];
-                *array[j+1] = temp;
-            }
-        }
-    }
-}
+void work_with_array(long long* restrict array[] , int* number_of_elements);
