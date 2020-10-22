97bec87782ac1e092100aac3610a19b126089a13
Mikhail Vintcukevich <mishavint@mail.ru>
2020-10-22 23:34:07 +0300

    Изменил почти всю структуру кода

--- src/main.c
+++ src/main.c
@@ -2,10 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
-#include <limits.h>
 #include "sort.h"
 
-int work_with_params (long long* restrict from , long long* restrict to , int number_of_params, char* params[] )
+struct exist_of_params
+{
+    int from;
+    int to;
+};
+
+int work_with_params (long long* restrict from , long long* restrict to ,
+                      int number_of_params, char* params[] , struct exist_of_params *existOfParams)
 {
     if(number_of_params < 2 ) // Если число параметров меньше, чем 2, то это ошибка (-1)
     {
@@ -22,49 +28,32 @@ int work_with_params (long long* restrict from , long long* restrict to , int nu
     }
 
 
-    int flag_for_from = 1 , flag_for_to = 1;
-    int second_flag_for_from = 1 , second_flag_for_to = 1;
     for (int i = 1 ; i < number_of_params ; ++i)                                        // Прогоняем циклом все параметры, которые получили из консоли
     {
             if( (*(from) == 0) && (strncmp(params[i]  , "--from=" , 7) == 0))     // Если нашли параметр, где начало строки совпадает с "--from=" то надо вычислить то, что нам надо из этого параметра
         {
             *(from) = strtoll(params[i] +7 , NULL, 10 );  // Вбиваем число в переменную
 
-            flag_for_from *=2;                            // Если пользователь не вводил параметр from, то флаг будет равен 2
-            if((strncmp( params[i] +7  , "0" , 1) == 0 ) ) // Если пользователь намеренно ввёл 0, то нам нужно увеличть переменную, чтобы потом нормально с этим работать
-            {
-                second_flag_for_from *= 2;
-            }
+            existOfParams -> from = 1;
         }
         else if( (*(to) == 0)  && (strncmp( params[i]  , "--to=" , 5) == 0))    // Если нашли параметр, где начало строки совпадает с "--to=" то надо вычислить то, что нам надо из этого параметра
         {
             *(to) = strtoll(params[i] +5 , NULL, 10 );          // Вбиваем число в переменную
 
-            flag_for_to *=2;                                    // Если пользователь не вводил параметр to, то флаг будет равен 2
-            if((strncmp( params[i] +5  , "0" , 1) == 0) )       // Если пользователь намеренно ввёл 0, то нам нужно увеличть переменную, чтобы потом нормально с этим работать
-            {
-                second_flag_for_to *=2;
-            }
+            existOfParams -> to = 1;
         }
     }
 
-    if(((second_flag_for_from == 1 ) && (second_flag_for_to) ==1 ) && ((*(from) == 0) && (*(to) == 0) )) // Если 2 раза было введено некоректное число, то это ошибка (-4)
+    if( (existOfParams -> from == 0) && (existOfParams -> to == 0)  ) // Если 2 раза было введено некоректное число, то это ошибка (-4)
     {
         return -4;
-    }
-    else if(flag_for_from == 1 && ((*(to) != 0) || (second_flag_for_to == 2)))       // Если пользователь намеренно не ввёл переменную, то нужно присвоить ей минимально возможное значение
-    {
-        *(from) = LLONG_MIN;
-    }
-    else if(flag_for_to == 1 && ((*(from) != 0) || (second_flag_for_from == 2)))     // Если пользователь намеренно не ввёл переменую, то нужно ей присвоить максимально возможное значение
-    {
-        *(to) = LLONG_MAX;
     }
 
     return 0;           // Если всё прошло успешно, возвращаем 0
 }
 
-int string_to_array(long long* restrict from ,long long* restrict to, long long* restrict array , long long* restrict array2 , int max_size_of_array)
+int string_to_array(long long* restrict from ,long long* restrict to, long long* restrict array ,
+                    long long* restrict array2 , int max_size_of_array , struct exist_of_params *existOfParams)
 {
     int elements_in_array = 0;                                                          // Переменная, обозначающа яколичество цифр в массиве
     long long number;                                                                   // Временное число, которые мы проверим на принадлежность к (from , to)
@@ -77,15 +66,16 @@ int string_to_array(long long* restrict from ,long long* restrict to, long long*
             fprintf(stderr , "can't read [%d] element" , elements_in_array);
             return -1;
         }
-        if(number <= *from )
+        if(number <= *from && existOfParams ->from != 0 )
         {
             fprintf(stdout, "%lli " , number);
         }
-        if(number >= *to)
+        if(number >= *to && existOfParams->to != 0)
         {
             fprintf(stderr , "%lli " , number);
         }
-        if((number > *from) && (number < *to))                                     // Если число спокойно прошло через все ошибки, то вбиваем его в массив
+        if(((number > *from) || (existOfParams -> from == 0 ))
+        && ((number < *to) || existOfParams -> to == 0))                                     // Если число спокойно прошло через все ошибки, то вбиваем его в массив
         {
             array[elements_in_array] = number;
             array2[elements_in_array] = number;
@@ -101,14 +91,15 @@ int main(int argc, char** argv) {
     long long int from = 0, to  = 0;                                  // Заполняем переменные нулями, пригодится дальшe
     long long int array[100];
     long long int copy_of_first_array [100];
+    struct exist_of_params existOfParams = {0 , 0};
 
-    int result_of_work = work_with_params(&from , &to , argc , argv );      // Создал функцию, которая заполняет переменные и возвращает возможные ошибки
+    int result_of_work = work_with_params(&from , &to , argc , argv , &existOfParams );      // Создал функцию, которая заполняет переменные и возвращает возможные ошибки
     if(result_of_work != 0 )                                                // Если всё-таки что-то пошло не так, корректно вывести ошибку
     {
         return result_of_work;                                              // Возвращаем ошибку и завершаем работу
     }
 
-    int elements_in_array = string_to_array(&from , &to, array , copy_of_first_array , 100);        // Получаем количество цифр(элементов) в строке, параллельно заполняя массив цифрами
+    int elements_in_array = string_to_array(&from , &to, array , copy_of_first_array , 100 , &existOfParams);        // Получаем количество цифр(элементов) в строке, параллельно заполняя массив цифрами
     if(elements_in_array < 0 )
     {
         return -5;
